be82d842569ec72f23a72cb849798a08e05b270d
Souheab Rehan <souheab@protonmail.com>
2022-07-14 15:13:09 +0300

    made changes to tags

--- main/rules.lua
+++ main/rules.lua
@@ -78,8 +78,8 @@ function _M.get(clientkeys, clientbuttons)
     },
 
     -- Set Firefox to always map on the tag named "2" on screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { screen = 1, tag = "2" } },
+    { rule = { instance = "Firefox" },
+    properties = { screen = 1, tag = RC.tags[1] } },
 
   }
 
--- main/tags.lua
+++ main/tags.lua
@@ -11,7 +11,7 @@ function _M.get ()
   awful.screen.connect_for_each_screen(function(s)
     -- Each screen has its own tag table.
     tags[s] = awful.tag(
-      {"", "", "", "ﱘ"}, s, RC.layouts[1]
+      {"", "", "", "ﱘ",""}, s, RC.layouts[1]
     )
   end)
   
